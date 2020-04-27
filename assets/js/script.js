ff1b317e20314f1390a51244735d1f45146803bc
Zishan Ansari <zshan.ansari07@gmail.com>
2020-04-27 14:50:12 +0530

    Implemented the functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 // Javascript here
+var timer;
+clearInterval(timer);
+timer = setInterval(function () {
+  var currentDate = new Date();
+  var currentTime = currentDate.toLocaleTimeString();
+  var displayClock = document.getElementById('digital-clock');
+  displayClock.innerHTML = currentTime;
+});
